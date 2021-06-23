c02b5b0783f3e9ad338dca845eecc10fe6c50691
santimontero <smontero@frba.utn.edu.ar>
2021-06-23 19:30:21 -0300

    agregando cosas

--- js/app.js
+++ js/app.js
@@ -9,3 +9,37 @@ btnToggle.addEventListener('click', function () {
 });
 //
 
+class Usuario{
+    constructor(nombreApellido, mail) {
+        this.nombreApellido = nombreApellido;
+        this.mail = mail;
+    }
+    msjBienvenida(){
+        alert(`Tus datos se guardaron correctamente `+ this.nombreApellido);
+    }
+}
+// const usuario1 = new Usuario ("Santi Montero", "santi@gmail.com");
+// usuario1.msjBienvenida();
+
+let usuarioNombre = prompt("Ingrese su nombre");
+let usuarioMail = prompt("Ingrese su mail");
+const usuarioNuevo = new Usuario (usuarioNombre,usuarioMail);
+console.log(usuarioNuevo);
+
+class Prestamo{
+    constructor(plataPedida){
+        this.plataPedida = parseFloat(plataPedida);
+        this.aprobado = false;
+    }
+    sumarInteres() {
+        this.plataPedida = this.plataPedida + (this.plataPedida * 0.3) ;
+    }
+    aprobarPrestamo() {
+        this.aprobado = true;
+    }
+}
+
+const prestamo1 = new Prestamo ("500");
+prestamo1.sumarInteres();
+prestamo1.aprobarPrestamo();
+console.log(prestamo1);
