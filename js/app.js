2961df7050ca23fe05096001208b6095b97bde0d
santimontero <smontero@frba.utn.edu.ar>
2021-06-28 22:24:08 -0300

    agrego funciones, clases de objetos

--- js/app.js
+++ js/app.js
@@ -8,11 +8,38 @@ btnToggle.addEventListener('click', function () {
     console.log(document.getElementById('sidebar'))
 });
 //
+class Moneda{
+    constructor(nombreMoneda,origen,cambioConElPeso){
+        this.nombreMoneda = nombreMoneda;
+        this.origen = origen;
+        this.cambioConElPeso = cambioConElPeso;
+    }
+    consultar(monto){
+        let total = monto * this.cambioConElPeso ;
+        console.log(`${monto} serian ${total} en la moneda ${this.nombreMoneda}`)
+    }
+}
+
+
+const dolar = new Moneda ("Dolar", "EEUU", 95.52);
+const libra = new Moneda ("Libra", "Reino Unido", 132.47);
+const yenJapones = new Moneda ("Yen japonés", "Japon", 0.86);
+
+dolar.consultar(100);
+libra.consultar(100);
+yenJapones.consultar(100);
+
+const monedas = [dolar,libra,yenJapones];
+
+
 class Usuario{
-    constructor(nombreApellido, mail) {
+    constructor(nombreApellido, mail, plata) {
         this.nombreApellido = nombreApellido;
         this.mail = mail;
-        // this.plata = 0;
+        this.plata = plata;
+    }
+    ingresarPlata(monto){
+        this.plata = this.plata + monto;
     }
     msjBienvenida(){
         alert(`Tus datos se guardaron correctamente `+ this.nombreApellido);
@@ -22,10 +49,14 @@ class Usuario{
 // usuario1.msjBienvenida();
 
 let usuarioNombre = prompt("Ingrese su nombre");
-let usuarioMail = prompt("Ingrese su mail");
-const usuarioNuevo = new Usuario (usuarioNombre,usuarioMail);
+let usuarioMail =  prompt("Ingrese su mail");
+let usuarioPlata = Number( prompt("Ingrese su plata"));
+const usuarioNuevo = new Usuario (usuarioNombre,usuarioMail,usuarioPlata);
 console.log(usuarioNuevo);
 
+const santi = new Usuario ("santi", "santi@gmail.com", 1000);
+const gero = new Usuario ("gero", "gero@gmail.com", 8000);
+const mati= new Usuario ("mati", "mati@gmail.com", 5000);
 class Prestamo{
     constructor(plataPedida){
         this.plataPedida = parseFloat(plataPedida);
@@ -39,9 +70,24 @@ class Prestamo{
     }
 }
 
+usuarioNuevo.ingresarPlata(2000);
+console.log(usuarioNuevo);
 const prestamo1 = new Prestamo ("500");
 prestamo1.sumarInteres();
 prestamo1.aprobarPrestamo();
 console.log(prestamo1);
 
-const arrayUsuarios = [usuarioNuevo]
+const usuarios = [usuarioNuevo,santi,gero,mati];
+console.log(usuarios);
+
+let deMasRicoAMasPobre = usuarios.sort(function(a,b){
+    return b.plata - a.plata;
+})
+console.log(deMasRicoAMasPobre);
+
+let nombreUsuario = prompt("Cuanta plata tiene el usuario : ");
+const buscarA = (nombreDelCliente) =>{
+    let a = usuarios.find(usuarios => usuarios.nombreApellido == nombreDelCliente);
+    console.log(a.plata);
+} 
+buscarA(nombreUsuario);
