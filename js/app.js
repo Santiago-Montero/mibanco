b249b2a0d2011e6190d68644a10d3d62afae139d
santimontero <smontero@frba.utn.edu.ar>
2021-06-29 13:55:06 -0300

    mejorando el codigo

--- js/app.js
+++ js/app.js
@@ -19,19 +19,6 @@ class Moneda{
         console.log(`${monto} serian ${total} en la moneda ${this.nombreMoneda}`)
     }
 }
-
-
-const dolar = new Moneda ("Dolar", "EEUU", 95.52);
-const libra = new Moneda ("Libra", "Reino Unido", 132.47);
-const yenJapones = new Moneda ("Yen japonés", "Japon", 0.86);
-
-dolar.consultar(100);
-libra.consultar(100);
-yenJapones.consultar(100);
-
-const monedas = [dolar,libra,yenJapones];
-
-
 class Usuario{
     constructor(nombreApellido, mail, plata) {
         this.nombreApellido = nombreApellido;
@@ -45,18 +32,6 @@ class Usuario{
         alert(`Tus datos se guardaron correctamente `+ this.nombreApellido);
     }
 }
-// const usuario1 = new Usuario ("Santi Montero", "santi@gmail.com");
-// usuario1.msjBienvenida();
-
-let usuarioNombre = prompt("Ingrese su nombre");
-let usuarioMail =  prompt("Ingrese su mail");
-let usuarioPlata = Number( prompt("Ingrese su plata"));
-const usuarioNuevo = new Usuario (usuarioNombre,usuarioMail,usuarioPlata);
-console.log(usuarioNuevo);
-
-const santi = new Usuario ("santi", "santi@gmail.com", 1000);
-const gero = new Usuario ("gero", "gero@gmail.com", 8000);
-const mati= new Usuario ("mati", "mati@gmail.com", 5000);
 class Prestamo{
     constructor(plataPedida){
         this.plataPedida = parseFloat(plataPedida);
@@ -69,25 +44,46 @@ class Prestamo{
         this.aprobado = true;
     }
 }
+const dolar = new Moneda ("Dolar", "EEUU", 95.52);
+const libra = new Moneda ("Libra", "Reino Unido", 132.47);
+const yenJapones = new Moneda ("Yen japonés", "Japon", 0.86);
+const monedas = [dolar,libra,yenJapones];
 
-usuarioNuevo.ingresarPlata(2000);
-console.log(usuarioNuevo);
+const santi = new Usuario ("santi", "santi@gmail.com", 1000);
+const gero  = new Usuario ("gero", "gero@gmail.com", 8000);
+const mati  = new Usuario ("mati", "mati@gmail.com", 5000);
+const juan  = new Usuario ("juan", "juani@gmail.com", 0);
+
+
+let usuarioNombre = prompt("Ingrese su nombre");
+let usuarioMail =  prompt("Ingrese su mail");
+let usuarioPlata = Number( prompt("Ingrese su plata"));
+const usuarioNuevo = new Usuario (usuarioNombre,usuarioMail,usuarioPlata);
+
+const usuarios = [usuarioNuevo,santi,gero,mati,juan];
+
+dolar.consultar(100);
+libra.consultar(100);
+yenJapones.consultar(100);
+
+juan.ingresarPlata(2000);
 const prestamo1 = new Prestamo ("500");
 prestamo1.sumarInteres();
 prestamo1.aprobarPrestamo();
 console.log(prestamo1);
-
-const usuarios = [usuarioNuevo,santi,gero,mati];
-console.log(usuarios);
 
 let deMasRicoAMasPobre = usuarios.sort(function(a,b){
     return b.plata - a.plata;
 })
 console.log(deMasRicoAMasPobre);
 
-let nombreUsuario = prompt("Cuanta plata tiene el usuario : ");
-const buscarA = (nombreDelCliente) =>{
-    let a = usuarios.find(usuarios => usuarios.nombreApellido == nombreDelCliente);
-    console.log(a.plata);
+let nombreConsulta = prompt("Ingrese el nombre para consultar el saldo");
+const buscarSaldo = (cliente) => {
+    let clienteEncontrado = usuarios.find(usuarios => usuarios.nombreApellido == cliente);
+    if(clienteEncontrado){
+        console.log(`el usuario ${clienteEncontrado.nombreApellido} tiene ${clienteEncontrado.plata}`);
+    }else{
+        console.log(`No existe ese usuario`);
+    }
 } 
-buscarA(nombreUsuario);
+buscarSaldo(nombreConsulta);
