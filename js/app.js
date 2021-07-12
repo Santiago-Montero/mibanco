b6246e5cbf493f80dea996e7c9330a443ce0a9b8
santimontero <smontero@frba.utn.edu.ar>
2021-07-12 12:12:20 -0300

    agrego nuevas cosas

--- js/app.js
+++ js/app.js
@@ -1,27 +1,23 @@
-
-//animacion- barra lateral
-const btnToggle = document.querySelector('.toggle-btn');
-
-btnToggle.addEventListener('click', function () {
-    console.log('clik')
-    document.getElementById('sidebar').classList.toggle('active');
-    console.log(document.getElementById('sidebar'))
-});
-//
 class Moneda{
-    constructor(nombreMoneda,origen,cambioConElPeso){
+    constructor(nombreMoneda,origen,ventaPorPeso,compraPorElPeso){
         this.nombreMoneda = nombreMoneda;
         this.origen = origen;
-        this.cambioConElPeso = cambioConElPeso;
+        this.ventaConElPeso = ventaPorPeso;
+        this.compraConElPeso = compraPorElPeso;
     }
-    consultar(monto){
-        let total = monto * this.cambioConElPeso ;
+    venta(monto){
+        let total = monto * this.ventaPorPeso ;
         console.log(`${monto} serian ${total} en la moneda ${this.nombreMoneda}`)
+    }
+    compra(monto){
+        let total = monto * this.compraPorElPeso ;
+        console.log(`${monto} en ${this.nombreMoneda} serian ${total} pesos`)
     }
 }
 class Usuario{
-    constructor(nombreApellido, mail, plata) {
-        this.nombreApellido = nombreApellido;
+    constructor(nombre, apellido, mail, plata) {
+        this.nombre = nombre;
+        this.apellido = apellido
         this.mail = mail;
         this.plata = plata;
     }
@@ -29,7 +25,7 @@ class Usuario{
         this.plata = this.plata + monto;
     }
     msjBienvenida(){
-        alert(`Tus datos se guardaron correctamente `+ this.nombreApellido);
+        alert(`Tus datos se guardaron correctamente `+ this.nombre);
     }
 }
 class Prestamo{
@@ -44,43 +40,47 @@ class Prestamo{
         this.aprobado = true;
     }
 }
-/*
-let botonInicioS = document.getElementById("btnInicioSesion")
-botonInicioS.addEventListener("click", guardarDatos)
 
-function guardarDatos(){
-    let nomApe = document.getElementById("nombreApellido").value ;
-    let mail = document.getElementById("mail").value ;
-    const usuarioNuevo = new Usuario (nomApe,mail,0);
-    console.log(` ACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ${usuarioNuevo}`);
-}
-guardarDatos();
-*/
-
-const dolar = new Moneda ("Dolar", "EEUU", 95.52);
-const libra = new Moneda ("Libra", "Reino Unido", 132.47);
-const yenJapones = new Moneda ("Yen japonés", "Japon", 0.86);
+const dolar = new Moneda ("Dolar", "EEUU", 95.52, 90);
+const libra = new Moneda ("Libra", "Reino Unido", 132.47, 127);
+const yenJapones = new Moneda ("Yen japonés", "Japon", 0.86, 0.40);
 const monedas = [dolar,libra,yenJapones];
 
-const santi = new Usuario ("santi", "santi@gmail.com", 1000);
-const gero  = new Usuario ("gero", "gero@gmail.com", 8000);
-const mati  = new Usuario ("mati", "mati@gmail.com", 5000);
-const juan  = new Usuario ("juan", "juani@gmail.com", 5);
+const santi = new Usuario ("santi","santi", "santi@gmail.com", 1000);
+const gero  = new Usuario ("gero","gero", "gero@gmail.com", 8000);
+const mati  = new Usuario ("mati","mati", "mati@gmail.com", 5000);
+const juan  = new Usuario ("juan","juan", "juani@gmail.com", 5);
 
+let botonInicioS = document.getElementById("btnInicioSesion");
 
-// let usuarioNombre = prompt("Ingrese su nombre");
-// let usuarioMail =  prompt("Ingrese su mail");
-// let usuarioPlata = Number( prompt("Ingrese su plata"));
+if (botonInicioS){
+    botonInicioS.addEventListener("click", mostrar);
+}
 
-//const usuarioNuevo = new Usuario (usuarioNombre,usuarioMail,usuarioPlata);
+function mostrar(e) {
+    e.preventDefault()
+    let nombre = document.getElementById("nombre").value
+    let apellido = document.getElementById("apellido").value
+    let mail = document.getElementById("mail").value
+    guardarDatos(nombre, apellido, mail)
+    alert(`Muchas gracias ${nombre} ${apellido} te registrate con el mail ${mail}`);
+}
+function guardarDatos(nombre, apellido, mail) {
+    const nuevoUsu = new Usuario (nombre, apellido, mail, 0)
+    localStorage.setItem("usuarioNuevo", JSON.stringify(nuevoUsu))
+}
+let datosUsu = JSON.parse(localStorage.getItem("usuarioNuevo"))
 
-//const usuarios = [usuarioNuevo,santi,gero,mati,juan];
+const usuarios = [santi,gero,mati,juan,datosUsu];
 
-const usuarios = [santi,gero,mati,juan];
-
-dolar.consultar(100);
-libra.consultar(100);
-yenJapones.consultar(100);
+// banco Vende
+dolar.venta(100);
+libra.venta(100);
+yenJapones.venta(100);
+//banco Compra
+dolar.compra(100);
+libra.compra(100);
+yenJapones.compra(100);
 
 juan.ingresarPlata(2000);
 const prestamo1 = new Prestamo ("500");
@@ -88,27 +88,39 @@ prestamo1.sumarInteres();
 prestamo1.aprobarPrestamo();
 console.log(prestamo1);
 
-let deMasRicoAMasPobre = usuarios.sort(function(a,b){
-    return b.plata - a.plata;
-})
-console.log(deMasRicoAMasPobre);
+// ORDENAR DE MAS PLATA A MENOS
+// let deMasRicoAMasPobre = usuarios.sort(function(a,b){
+//     return b.plata - a.plata;
+// })
+// console.log(deMasRicoAMasPobre);
 
-//let nombreConsulta = prompt("Ingrese el nombre del usuario para consultar el saldo");
-const buscarSaldo = (cliente) => {
-    let clienteEncontrado = usuarios.find(usuarios => usuarios.nombreApellido == cliente);
-    if(clienteEncontrado){
-        console.log(`el usuario ${clienteEncontrado.nombreApellido} tiene ${clienteEncontrado.plata}`);
-    }else{
-        console.log(`No existe ese usuario`);
-    }
-} 
+
+// BUSCAR USUARIO
+//let nombreConsulta = prompt("Ingrese el nombre del usuario para venta el saldo");
+// const buscarSaldo = (cliente) => {
+//     let clienteEncontrado = usuarios.find(usuarios => usuarios.nombreApellido == cliente);
+//     if(clienteEncontrado){
+//         console.log(`el usuario ${clienteEncontrado.nombreApellido} tiene ${clienteEncontrado.plata}`);
+//     }else{
+//         console.log(`No existe ese usuario`);
+//     }
+// } 
 // buscarSaldo(nombreConsulta);
-
-let infoUsuario = document.getElementById("usuarios");
-for (const usu of usuarios){
-    let contenedor = document.createElement("tr");
-    contenedor.innerHTML = `<td>${usu.nombreApellido}</td> 
-                            <td>${usu.mail}</td> 
-                            <td>${usu.plata}</td>`
-    infoUsuario.appendChild(contenedor);
+let mostrarHisotrial = document.getElementById("btnMostrarHistorial");
+if (mostrarHisotrial){
+    mostrarHisotrial.addEventListener("click", mostrarHistorial);
 }
+function mostrarHistorial(){
+    document.getElementById("usuarioHistorial").style.display = "block"
+    let infoUsuario = document.getElementById("usuarios");
+    for (const usu of usuarios){
+        let contenedor = document.createElement("tr");
+        contenedor.innerHTML = `<td>${usu.nombre}</td> 
+                                <td>${usu.apellido}</td> 
+                                <td>${usu.mail}</td> 
+                                <td>${usu.plata}</td>`
+        infoUsuario.appendChild(contenedor);
+    }
+}
+
+
